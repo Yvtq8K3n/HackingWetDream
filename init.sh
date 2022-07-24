6c43d7c5d772b072e5c474b4686a51b964e960da
Yvtq8K3n <Yvtq8K3n@gmail.com>
2022-07-24 14:35:40 -0400

    Updating tmux config

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-TMUX_CORE = https://gist.githubusercontent.com/Yvtq8K3n/fb7b9590aca6a67b7a023d4faa4540b4/raw/3becbcbe9d47447af7539f2e0f34e28d9aa6f141/.tmux.conf
-TMUX_VPN = https://gist.githubusercontent.com/Yvtq8K3n/fb7b9590aca6a67b7a023d4faa4540b4/raw/3becbcbe9d47447af7539f2e0f34e28d9aa6f141/vpn.sh
+TMUX_CORE = "https://gist.githubusercontent.com/Yvtq8K3n/fb7b9590aca6a67b7a023d4faa4540b4/raw/b855fe0086ed0b849ba1845097b4481a7e034448/.tmux.conf"
+TMUX_VPN = "https://gist.githubusercontent.com/Yvtq8K3n/fb7b9590aca6a67b7a023d4faa4540b4/raw/b855fe0086ed0b849ba1845097b4481a7e034448/vpn.sh"
 
 curl -L $TMUX_CORE -o .tmux.conf
 chmod +x .tmux.conf
