15a7e73c63107ce0ff3fd0225261f64af0f5f47d
Yvtq8K3n <Yvtq8K3n@gmail.com>
2022-07-24 14:24:10 -0400

    Adding Script folder

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 TMUX_CORE = https://gist.githubusercontent.com/Yvtq8K3n/fb7b9590aca6a67b7a023d4faa4540b4/raw/3becbcbe9d47447af7539f2e0f34e28d9aa6f141/.tmux.conf
 TMUX_VPN = https://gist.githubusercontent.com/Yvtq8K3n/fb7b9590aca6a67b7a023d4faa4540b4/raw/3becbcbe9d47447af7539f2e0f34e28d9aa6f141/vpn.sh
 
@@ -13,8 +12,4 @@ mkdir -p ~/Scripts
 #Enter Scripts folder
 cd ~/Scripts
 curl -L $TMUX_VPN -o vpn.sh
-
-
-...
-Load all scripts in scripts folder
-ETC.
+curl -L https://github.com/Yvtq8K3n/HackingWetDream/archive/refs/heads/main.tar.gz | tar xz --wildcards "*/Scipts" --strip-components=3
