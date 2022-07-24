b4bc632884c7a8b6ee36a06c0c6eb4e141d01e31
Yvtq8K3n <Yvtq8K3n@gmail.com>
2022-07-24 19:43:52 -0400

    Amass minor changes

--- tools/amass.sh
+++ tools/amass.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+cd ~/tools
 
-echo "hello world"
+TARGET_DOMAIN=$1
+
+echo $TARGET_DOMAIN
+#Create go 
+curl -L https://go.dev/dl/go1.18.4.linux-amd64.tar.gz -o go.tar.gz
+tar xvf go.tar.gz
+sudo rm -rf /usr/local/go
+sudo mv go /usr/local/
+export PATH=$PATH:/usr/local/go/bin
+
+#Install amass
+sudo ap update -y
+sudo apt install amass -y
+
+#Use amass
+amass enum -d $TARGET_DOMAIN
