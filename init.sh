2b5d93b3b9ee058b44f1b47f2ce12ee62633b231
Yvtq8K3n <Yvtq8K3n@gmail.com>
2022-07-24 17:55:21 -0400

    Minor updates on init.sh

--- init.sh
+++ init.sh
@@ -3,18 +3,19 @@ TMUX_CORE="https://gist.githubusercontent.com/Yvtq8K3n/fb7b9590aca6a67b7a023d4fa
 TMUX_VPN="https://gist.githubusercontent.com/Yvtq8K3n/fb7b9590aca6a67b7a023d4faa4540b4/raw/19d6531bd14da1443b0ef63866a506857161d42d/vpn.sh"
 GIT_CORE="https://github.com/Yvtq8K3n/HackingWetDream/archive/refs/heads/main.tar.gz"
 
+printf "#Installing clipboard utils for smooth tmux experience\n\n"
 sudo apt -y update
 sudo apt -y install xclip
 
 #Creates initial stucture
-printf "#Creating initial stucture\n"
+printf "\n#Creating initial stucture\n"
 mkdir -p ~/projects
 mkdir -p ~/scripts
 mkdir -p ~/tools
 
 printf " -Retreiving tmux conf from: ${TMUX_CORE}\n"
-curl -L $TMUX_CORE -o .tmux.conf --fail --silent --show-error
-chmod +x .tmux.conf
+curl -L $TMUX_CORE -o ~/.tmux.conf --fail --silent --show-error
+chmod +x ~/.tmux.conf
 
 #Enter Scripts folder
 printf "\n#Entering scripts folder\n"
@@ -25,6 +26,7 @@ curl -L $TMUX_VPN -o vpn.sh --fail --silent --show-error
 
 printf " -Retreiving all scripts from GIT: ${GIT_CORE}\n"
 curl -s -L $GIT_CORE --fail --silent --show-error | tar xz --wildcards "*/scripts/*" --strip-components 2
+printf " -Granting executable permission to all scripts\n"
 chmod +x *
 
 #Enter tools folder
@@ -33,3 +35,5 @@ cd ~/tools
 printf " -Retreiving all tool_scripts from GIT: ${GIT_CORE}\n"
 curl -s -L $GIT_CORE --fail --silent --show-error | tar xz --wildcards "*/tools/*" --strip-components 2
 
+printf " -Granting executable permission to all scripts\n"
+chmod +x *
