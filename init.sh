49e1183508e1057353e7e2c257ba29aa90f95354
Yvtq8K3n <Yvtq8K3n@gmail.com>
2022-07-24 17:33:38 -0400

    Fixing minor typo on sudo word

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ TMUX_CORE="https://gist.githubusercontent.com/Yvtq8K3n/fb7b9590aca6a67b7a023d4fa
 TMUX_VPN="https://gist.githubusercontent.com/Yvtq8K3n/fb7b9590aca6a67b7a023d4faa4540b4/raw/b855fe0086ed0b849ba1845097b4481a7e034448/vpn.sh"
 GIT_CORE="https://github.com/Yvtq8K3n/HackingWetDream/archive/refs/heads/main.tar.gz"
 
-sudp apt -y update
+sudo apt -y update
 sudo apt -y install xclip
 
 #Creates initial stucture
