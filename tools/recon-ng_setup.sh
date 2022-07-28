f49ad05ec548ea579200819d9e987e2ddebd284b
Yvtq8K3n <Yvtq8K3n@gmail.com>
2022-07-28 12:25:15 -0400

    Source folders will now start in upper-case

--- tools/recon-ng_setup.sh
+++ tools/recon-ng_setup.sh
@@ -7,8 +7,8 @@ if ! command -v recon-ng &> /dev/null
 then
     echo "Recon-ng not found, proceeding to install it"
     cd ~/tools
-    git clone https://github.com/lanmaster53/recon-ng.git reconng
-    ln -s ~/tools/reconng/recon-ng recon-ng
+    git clone https://github.com/lanmaster53/recon-ng.git Recon-ng
+    ln -s ~/tools/Recon-ng/recon-ng recon-ng
 fi
 echo "Recon-ng is ready to be use"
 
