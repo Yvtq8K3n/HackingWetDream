d59b44f961e81f67da945b3d01e9f45d46924f6d
yvtq8k3n <yvtq8k3n@gmail.com>
2022-08-06 19:13:24 -0400

    .Adding api-keys for the harvester .Updating startRecon to iterate of api_keys

--- tools/theharvester_setup.sh
+++ tools/theharvester_setup.sh
@@ -11,8 +11,10 @@ then
     (cd TheHarvester && python3 -m pip install -r requirements/base.txt)
     ln -s ~/tools/TheHarvester/theHarvester.py theharvester
 
-    #Since theHarvester has the paths hardcoded i just replaced them:) to point to the right direction, h5! :)
+    #Since theHarvester uses hardcoded paths, i just changed it to point to the right direction, h5! :)
+    sed -i "s@\/etc\/theHarvester\/api-keys.yaml/@\/tmp\/theharvester.yaml@g" ~/tools/TheHarvester/theHarvester/lib/core.py
     sed -i "s@\/etc\/theHarvester\/@$HOME/tools/TheHarvester\/@g" ~/tools/TheHarvester/theHarvester/lib/core.py
+
 fi
 echo "TheHarvester is ready to be use"
 
