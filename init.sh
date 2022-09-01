94afcd4d46821ee96f2909875bb223f2eea79a1a
João Marques <Yvtq8K3n@gmail.com>
2022-09-01 22:49:28 +0100

    Update init.sh
    Making tmux easily installable whitout running full script

--- init.sh
+++ init.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-TMUX_CORE="https://gist.githubusercontent.com/Yvtq8K3n/fb7b9590aca6a67b7a023d4faa4540b4/raw/19d6531bd14da1443b0ef63866a506857161d42d/.tmux.conf"
-TMUX_VPN="https://gist.githubusercontent.com/Yvtq8K3n/fb7b9590aca6a67b7a023d4faa4540b4/raw/19d6531bd14da1443b0ef63866a506857161d42d/vpn.sh"
-GIT_CORE="https://github.com/Yvtq8K3n/HackingWetDream/archive/refs/heads/main.tar.gz"
-API_CONFIG_FILE="https://github.com/Yvtq8K3n/HackingWetDream/raw/main/config.yaml"
-
-printf "#Installing clipboard utils for smooth tmux experience\n\n"
 sudo apt -y update
-sudo apt -y install xclip
 
 #Creates initial stucture
 printf "\n#Creating initial stucture\n"
@@ -15,16 +8,19 @@ mkdir -p ~/scripts
 mkdir -p ~/tools
 mkdir -p ~/templates
 
+export GIT_CORE="https://github.com/Yvtq8K3n/HackingWetDream/archive/refs/heads/main.tar.gz"
+export API_CONFIG_FILE="https://github.com/Yvtq8K3n/HackingWetDream/raw/main/config.yaml"
+export TMUX_CORE="https://gist.githubusercontent.com/Yvtq8K3n/fb7b9590aca6a67b7a023d4faa4540b4/raw/19d6531bd14da1443b0ef63866a506857161d42d/.tmux.conf"
+export TMUX_VPN="https://gist.githubusercontent.com/Yvtq8K3n/fb7b9590aca6a67b7a023d4faa4540b4/raw/19d6531bd14da1443b0ef63866a506857161d42d/vpn.sh"
 printf " -Retreiving tmux conf from: ${TMUX_CORE}\n"
 curl -L $TMUX_CORE -o ~/.tmux.conf --fail --silent --show-error
 chmod +x ~/.tmux.conf
 
-#Enter Scripts folder
-printf "\n#Entering scripts folder\n"
-cd ~/scripts
-
 printf " -Retreiving vpn script for tmux from: ${TMUX_VPN}\n"
-curl -L $TMUX_VPN -o vpn.sh --fail --silent --show-error
+(mkdir -p ~/scripts; cd ~/scripts; curl -L $TMUX_VPN -o vpn.sh --fail --silent --show-error; chmod +x vpn.sh)
+
+printf "#Installing clipboard utils for smooth tmux experience\n\n"
+sudo apt -y install xclip
 
 printf " -Retreiving all scripts from GIT: ${GIT_CORE}\n"
 curl -s -L $GIT_CORE --fail --silent --show-error | tar xz --wildcards "*/scripts/*" --strip-components 2
